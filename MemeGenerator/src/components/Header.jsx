5c258e25f43038537a51922babe9845094e496f8
uzairwaheed-8 <uwaheed.ce43ceme@student.nust.edu.pk>
2024-07-08 00:56:07 +0500

    Add files via upload

---
+++ MemeGenerator/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZeseYXIURcTZUMXLNi_EgAhjo9nuvqnf6jw&s"
+                className="header--image"
+                alt="Meme logo"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
