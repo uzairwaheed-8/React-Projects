4d0edab3a2b7c5a72a0577f05ac14ddf2b3f8dbe
uzairwaheed-8 <uwaheed.ce43ceme@student.nust.edu.pk>
2024-07-15 11:01:19 +0500

    App.js

--- ReactFacts_Dark--light mode/App.js
+++ ReactFacts_Dark--light mode/App.js
@@ -3,10 +3,11 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [darkMode,setDarkMode] = React.useState(false)
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar darkMode={darkMode} setDarkMode={setDarkMode}/>
+            <Main darkMode={darkMode}/>
         </div>
     )
 }
